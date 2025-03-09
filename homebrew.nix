c7eb4c42c53ca4b1b6898ce7bb7c8cbf7db51371
Naga Srinath <nagasrinath@pm.me>
2025-03-09 21:07:43 +0530

    I'm tired

---
+++ homebrew.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }: {
+  homebrew = {
+    enable = true;
+    global.brewfile = true;
+    onActivation = {
+      cleanup = "uninstall";
+      autoUpdate = true;
+      upgrade = true;
+    };
+    taps = [ "nikitabobko/tap" ];
+    brews = [ "mas" ];
+    masApps = {
+      "Simplenote" = 692867256;
+      "Photomator" = 1444636541;
+      "Wireguard" = 1451685025;
+      "Davinci Resolve" = 571213070;
+      "Amphetamine" = 937984704;
+    };
+    casks = [
+      "ghostty" "librewolf" "ollamac" "jan" "vivaldi" "raycast"
+      "intellij-idea" "pycharm" "clion" "vmware-fusion"
+      "proton-pass" "proton-mail-bridge" "proton-drive" "protonvpn"
+      "proton-mail" "whatsapp" "telegram" "arc" "brave-browser"
+      "iina" "discord" "signal" "element" "dbeaver-community"
+      "inkscape" "zed" "zed@preview" "vscodium" "utm" "gimp"
+      "spotify" "zen-browser" "audacity" "transmission" "wireshark"
+      "tailscale" "android-studio" "obs" "chatgpt" "orion"
+      "eloston-chromium" "postman" "vlc" "hiddenbar" "cyberduck"
+      "playcover-community" "openvpn-connect" "jordanbaird-ice"
+      "fleet" "steam" "motrix" "aerospace"
+    ];
+  };
+}
