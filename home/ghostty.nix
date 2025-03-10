d7d12700ca66288fb4c811560f0958eafbe5f9fa
Naga Srinath <nagasrinath@pm.me>
2025-03-10 22:47:05 +0530

    Added Ghostty config

---
+++ home/ghostty.nix
@@ -0,0 +1,18 @@
+{ ... }: {
+  programs.ghostty = {
+    # enable = true;
+    enableFishIntegration = true;
+    settings = {
+      theme = "GruvboxDarkHard";
+      font-family = "Inconsolata Nerd Font";
+      font-size = 15;
+      cursor-style = "underline";
+      window-padding-x = 4;
+      window-padding-y = 4;
+      window-padding-balance = true;
+      macos-titlebar-proxy-icon = "hidden";
+      macos-titlebar-style = "transparent";
+      term = "xterm-256color";
+    };
+  };
+}
