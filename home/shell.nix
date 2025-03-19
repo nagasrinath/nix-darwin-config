5877388f0fb4d7302b24ad88c4c2af9f8b01a252
Naga Srinath <nagasrinath@proton.me>
2025-03-19 20:42:42 +0530

    Daily updates

--- home/shell.nix
+++ home/shell.nix
@@ -1,12 +1,12 @@
 {...}: {
   programs.fish = {
     enable = true;
-    interactiveShellInit = "set fish_greeting";
+    interactiveShellInit = "
+      set fish_greeting
+      export MANPAGER='nvim +Man!'";
     shellAliases = {
       "..." = "cd ../..";
-      "vim" = "hx";
       "ff" = "fastfetch";
-      "ping" = "gping";
     };
   };
 }
